6be669e2fff2255d59cc4da35ee81920fd098e12
gilad053 <120463553+gilad053@users.noreply.github.com>
2025-04-09 15:29:44 +0300

    Update q1.sv
    first complete FSM checking insights and correctnes in chatGPT

--- Homework/ex_2/q1.sv
+++ Homework/ex_2/q1.sv
@@ -13,3 +13,49 @@ module (
   always_comb begin
     next_state = current_state;
     case (current_state)
+      z_st: begin
+        if(in==1'b1) begin
+          next_state = o_st;
+        end
+      end
+      o_st: begin
+        if(in==1'b0) begin
+          next_state = oz_st;
+        end
+      end
+      oz_st: begin
+        if(in==1'b1) begin
+          next_state = ozo_st;
+        end
+        else if(in==1'b0) begin
+          next_state = z_st;
+        end
+      end
+      ozo_st: begin
+        if(in==1'b1) begin
+          next_state = ozoo_st;
+        end
+        else if(in==1'b0) begin
+          next_state = oz_st;
+        end
+      end
+      ozoo_st: begin
+        if(in==1'b1) begin
+          next_state = o_st;
+        end
+        else if(in==1'b0) begin
+          next_state = ozooz_st;
+        end
+      end
+      ozooz_st: begin
+        out = 1'b1;
+        if(in==1'b1) begin
+          next_state = ozo_st;
+        end
+        else if(in==1'b0) begin
+          next_state = z_st;
+        end
+      end
+    endcase
+  end
+endmodule

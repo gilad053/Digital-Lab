d39a817dbadf97a4339250cfc7d0cea746378e96
gilad053 <120463553+gilad053@users.noreply.github.com>
2025-04-09 15:00:39 +0300

    Update q1.sv

--- Homework/ex_2/q1.sv
+++ Homework/ex_2/q1.sv
@@ -3,3 +3,13 @@ module (
   output logic out
 );
   
+  typedef enum {z_st,o_state,oz_st,ozo_st,ozoo_st,ozooz_st} sm_type; // z-stands for 0, o-stands for 1.
+  sm_type current_state;
+  sm_type next_state;
+  initial begin
+    current_state = z_st;
+    out = 1'b0;
+  end
+  always_comb begin
+    next_state = current_state;
+    case (current_state)
